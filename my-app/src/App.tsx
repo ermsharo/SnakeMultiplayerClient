db12c5770eebbbade38c2efee7d62077ba6c0cd0
emilio <ermsharo@gmail.com>
2022-11-01 04:24:57 -0300

    change states structure

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useRef, useState } from 'react';
+import styled from "styled-components";
+
+const Board = styled.div`
+  margin: auto;
+  width: 1000px;
+  height: 100%;
+  border: 2px solid red;
+`;
+
+
+const canvasX = 1000;
+const canvasY = 1000;
+
+const initialSnake = [
+  [4, 10],
+  [4, 10],
+];
+
+const initialApple = [14, 10];
+const scale = 50;
+const timeDelay = 100;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [snake, setSnake] = useState(initialSnake);
+  const [apple, setApple] = useState(initialApple);
+  const [direction, setDirection] = useState([0, -1]);
+  const [delay, setDelay] = useState<number | null>(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+
+  return (<Board>asdasdasd</Board>);
 }
 
 export default App;
