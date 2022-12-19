2532dd7070936e49c04dfbda1a60e929b3793306
emilio <ermsharo@gmail.com>
2022-12-19 02:31:45 -0300

    remove deprect node folder

--- my-app/src/components/Login/Login.tsx
+++ my-app/src/components/Login/Login.tsx
@@ -1,64 +1,121 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Button from "@mui/material/Button";
 
-const FormBox = styled.div`
-  width: 50vw;
-  margin: auto;
-  text-align: center;
+const BoardBox = styled.div`
+  width: 100%;
+  font-family: "Varela Round", sans-serif;
+  font-weight: 400;
+  height: 100%;
 `;
 
-const Title = styled.h2`
-  font-size: 18px;
+const LoginBox = styled.div`
+  padding-top: 32px;
+  grid-column: 2/6;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 16px;
+
+  input {
+    font-family: "Varela Round", sans-serif;
+  }
+  button {
+    font-family: "Varela Round", sans-serif;
+    font-weight: 700;
+    background-color: blue;
+    border-radius: 20px;
+    margin-top: 16px;
+
+    &:hover {
+      background-color: blue;
+      opacity: 0.9;
+    }
+  }
 `;
 
-const ButtonBox = styled.div`
+const LogoBox = styled.div`
+  display: flex;
+  justify-content: center;
   padding: 16px;
 `;
 
-const SingInButton = styled.button`
-  width: 300px;
-  background-color: blue;
-  font-family: "Press Start 2P", cursive;
-  padding: 8px;
-  color: white;
-  outline: none;
-
-  border: 1px solid transparent;
-  border-radius: 5px;
+const CreateAccountButton = styled.div`
+  font-family: "Varela Round", sans-serif;
+  font-weight: 600;
+  cursor: pointer;
+  margin-top: 16px;
+  padding-left: 8px;
+  &:hover {
+    opacity: 0.7;
+  }
 `;
 
 function Login() {
   const navigate = useNavigate();
-  const [userName, setUserName] = useState("");
+  const [formInputs, setFormInputs] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    localStorage.setItem("userName", userName);
-    // socket.emit("newUser", {userName, socketID: socket.id})
-    navigate("/game");
-  };
+  // const handleSubmit = (e: any) => {
+  //   e.preventDefault();
+  //   localStorage.setItem("userName", userName);
+  //   // socket.emit("newUser", {userName, socketID: socket.id})
+  //   navigate("/game");
+  // };
 
   return (
-    <FormBox>
-      <form className="home__container" onSubmit={handleSubmit}>
-        <Title> Snake multiplayer</Title>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          minLength={6}
-          name="username"
-          id="username"
-          className="username__input"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <ButtonBox>
-          {" "}
-          <SingInButton>SIGN IN</SingInButton>
-        </ButtonBox>
-      </form>
-    </FormBox>
+    <>
+    <BoardBox>
+      <LogoBox>
+        <Logo color="#00008b" size="6vw" />
+      </LogoBox>
+      <Grid>
+        <LoginBox>
+          <TextField
+            fullWidth
+            id="outlined-name"
+            label="Email"
+            name="email"
+            value={formInputs.email}
+            onChange={handleChange}
+          />
+          <TextField
+            type="password"
+            fullWidth
+            id="outlined-name"
+            label="Password"
+            name="password"
+            value={formInputs.password}
+            onChange={handleChange}
+          />
+   
+   
+
+
+          <Button
+            onClick={() => {
+              singIn();
+            }}
+            fullWidth
+            variant="contained"
+          >
+            Login
+          </Button>
+
+          <CreateAccountButton
+            onClick={() => {
+              navigate("create-account");
+            }}
+          >
+            Create account
+          </CreateAccountButton>
+        </LoginBox>
+      </Grid>
+    </BoardBox>
+  </>
   );
 }
 
