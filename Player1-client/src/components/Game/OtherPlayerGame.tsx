64e16c2207b9fc72d62920d3c77340a19f432daf
emilio <ermsharo@gmail.com>
2022-12-20 05:39:32 -0300

    remove insistent garbage

--- Player1-client/src/components/Game/OtherPlayerGame.tsx
+++ Player1-client/src/components/Game/OtherPlayerGame.tsx
@@ -1,30 +1,14 @@
-import { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { getUserInfo } from "./../../utils/storageManegement";
+import React, { useEffect, useRef, useState } from "react";
 
 const canvasX = 1000;
 const canvasY = 1000;
 
 const scale = 50;
 
-
-
-function OtherGamePlayer({ otherPlayerSnake, gameInfo }: any) {
+function OtherGamePlayer({ otherPlayerSnake }: any) {
   const canvasRefOtherPlayer = useRef<HTMLCanvasElement | null>(null);
-
-  let { id } = getUserInfo();
-
-  const getSnakerArray = () => {
-    console.log("Game info", gameInfo["playerOnePosition"]);
-    if (id === "1") {
-      return gameInfo["playerOnePosition"];
-    }
-    if (id === "2") {
-      return gameInfo["playerTwoPositon"];
-    }
-  };
-
-  console.log("get snakes array", getSnakerArray());
 
   useEffect(() => {
     if (canvasRefOtherPlayer.current) {
@@ -34,16 +18,17 @@ function OtherGamePlayer({ otherPlayerSnake, gameInfo }: any) {
         ctx.setTransform(scale, 0, 0, scale, 0, 0);
         ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
         ctx.fillStyle = "purple";
-        getSnakerArray().forEach(([x, y]: [any, any]) =>
-          ctx.fillRect(x, y, 1, 1)
-        );
+        if (otherPlayerSnake != null) {
+          otherPlayerSnake.forEach(([x, y]: [any, any]) =>
+            ctx.fillRect(x, y, 1, 1)
+          );
+        }
       }
     }
   }, [otherPlayerSnake]);
 
   // console.log("id ->", id)
   // console.log("game info from other" , gameInfo)
- 
 
   if (!otherPlayerSnake)
     return <div className="playArea">Sem representação do outro player</div>;
